ca7ed97aadc88359af0b41fa6d8488895421eaad
Jakub Kwaśniak <kwasniak.jakub@kompusoft.pl>
2022-06-23 12:33:50 +0200

    Dodaj pliki projektów.

---
+++ Modele/ModelPokazaniaKontaku.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace netPcListaKontaktow.Modele
+{
+    public class ModelPokazaniaKontaku
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(15, ErrorMessage = "Imie jest zbyt dlugie")]
+        [MinLength(5, ErrorMessage ="Imie jest zbyt krótkie")]
+        public string Imie { get; set; }
+        [Required]
+        [StringLength(15, ErrorMessage = "Nazwisko jest zbyt dlugie")]
+        [MinLength(5, ErrorMessage ="Nazwisko jest zbyt krótkie")]
+        public string Nazwisko { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string AdresEmail { get; set; }
+
+        public string Haslo { get; set; }
+
+        [StringLength(8, ErrorMessage = "Kategoria jest zbyt dlugie")]
+        public string Kategoria { get; set; }
+        public string NumerTelefonu { get; set; }
+        public DateTime DataUrodzenia { get; set; }
+
+        public ModelPokazaniaKontaku()
+        {
+            DataUrodzenia = DateTime.Now;
+        }
+
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,61 @@
+using BibliotekaDostepuDoDanych;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using netPcListaKontaktow.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace netPcListaKontaktow
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+            services.AddSingleton<WeatherForecastService>();
+            //Konfiguracja po³¹czenia z serwerem/Parametry Po³¹czenia
+            services.AddTransient<ISqlDostepDoDanych, SqlDostepDoDanych>();
+            services.AddTransient<IKontaktyDane, KontaktyDane>();
+            services.AddTransient<IDaneLogowania, DaneLogowania>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/_Host");
+            });
+        }
+    }
+}
